33ce477f687340673fa9791b33dc5aface3704d7
Ken Robertson <ken@apcera.com>
2015-10-30 17:22:39 -0700

    Added simple power command handler for reboot/poweroff operations.
    Added a small C executable that can be pulled into the console ACI image to
    properly handle halt/poweroff/reboot operation within a privileged container.

---
+++ util/power/power.c
@@ -0,0 +1,50 @@
+// Copyright 2015 Apcera Inc. All rights reserved.
+//
+// Simple executable to write to the sysreq-trigger handler to shutdown or
+// reboot the host from a privileged container.
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void writesysreq(char c) {
+	int fd = open("/host/proc/sysrq-trigger", O_WRONLY);
+	if (fd == -1) {
+    perror("open");
+    exit(1);
+  }
+	if (write(fd, &c, 1) < 0) {
+		perror("write");
+		exit(1);
+	}
+	if (close(fd) == -1) {
+		perror("close");
+		exit(1);
+	}
+}
+
+void poweroff() {
+	writesysreq('o');
+}
+
+void reboot() {
+	writesysreq('b');
+}
+
+int main(int argc, char **argv) {
+	switch (argv[0][0]) {
+	case 'h':
+		poweroff();
+		break;
+	case 'p':
+		poweroff();
+		break;
+	case 'r':
+		reboot();
+		break;
+	default:
+		fprintf(stderr, "unrecognized command\n");
+		exit(1);
+	}
+}
