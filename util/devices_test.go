75e7e7169ebc7ec39729a83895aeaa477eff59e4
Ken Robertson <ken@apcera.com>
2015-11-16 18:16:17 -0800

    Forgot to move these two tests

---
+++ util/devices_test.go
@@ -0,0 +1,21 @@
+// Copyright 2015 Apcera Inc. All rights reserved.
+
+package util
+
+import (
+	"testing"
+
+	tt "github.com/apcera/util/testtool"
+)
+
+func TestGetRawDevice(t *testing.T) {
+	tt.TestEqual(t, GetRawDevice("/dev/sda1"), "/dev/sda")
+	tt.TestEqual(t, GetRawDevice("/dev/nvme0n1p3"), "/dev/nvme0n1")
+}
+
+func TestGetPartitionNumber(t *testing.T) {
+	tt.TestEqual(t, GetPartitionNumber("/dev/sda1"), "1")
+	tt.TestEqual(t, GetPartitionNumber("/dev/sda3"), "3")
+	tt.TestEqual(t, GetPartitionNumber("/dev/nvme0n1p2"), "2")
+	tt.TestEqual(t, GetPartitionNumber("/dev/sda"), "")
+}
