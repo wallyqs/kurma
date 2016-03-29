7be83146e29b350bac05703f2a508aa8f6a58758
Ken Robertson <ken@apcera.com>
2016-03-29 16:36:50 -0700

    build: Setup loop0..7 since losetup is not looking at local devices

--- build/vms/setup_loopback.sh
+++ build/vms/setup_loopback.sh
@@ -1,11 +1,11 @@
 function setup_lookback() {
-  for i in $(seq 64 67); do
+  for i in $(seq 0 7); do
     mknod -m 0660 /dev/loop$i b 7 $i
   done
 }
 
 function free_looback() {
-  for i in $(seq 64 67); do
+  for i in $(seq 0 7); do
     losetup -d /dev/loop$i > /dev/null 2>&1 || true
   done
 }
