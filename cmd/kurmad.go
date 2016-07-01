e490c76844ebb2109709b0038a3bbadc28c3d44c
Waldemar Quevedo <wally@apcera.com>
2016-07-01 01:27:14 +0000

    Tweaks to logging around Kurma managers
    Changes format of logging string for kurmad, sets the pod field
    among other minor logging related changes.

--- cmd/kurmad.go
+++ cmd/kurmad.go
@@ -7,6 +7,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net/url"
 	"os"
 	"runtime"
 
@@ -14,12 +15,23 @@ import (
 	"github.com/apcera/logray"
 )
 
+const (
+	formatString = "%color:class%%year%-%month%-%day% %hour%:%minute%:%second%.%nanosecond% %tzoffset% %tz% [%class% pid=%pid% pod='%field:pod%' source='%sourcefile%:%sourceline%']%color:default% %message%"
+)
+
 func main() {
 	var configFile string
 	flag.StringVar(&configFile, "configFile", "kurmad.yml", "Path to the kurma configuration file")
 	flag.Parse()
 
-	logray.AddDefaultOutput("stdout://", logray.ALL)
+	u := url.URL{
+		Scheme: "stdout",
+		RawQuery: url.Values(map[string][]string{
+			"format": []string{formatString},
+		}).Encode(),
+	}
+
+	logray.AddDefaultOutput(u.String(), logray.ALL)
 
 	if err := kurmad.Run(configFile); err != nil {
 		fmt.Fprintf(os.Stderr, "Failure running process: %v\n", err)
